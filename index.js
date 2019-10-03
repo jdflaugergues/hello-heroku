7eda2f99230cd59f4b9811ca6e9be2eca4411f00
Jonathan de Flaugergues <jonathan.deflaugergues@abbeal.com>
2019-10-03 17:53:04 +0200

    add third feature

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello my world\n');
+  res.end('Hello my third world\n');
 });
 
 server.listen(PORT, () => {
