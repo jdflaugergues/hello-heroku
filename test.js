e31d0c6e6ec766997e60acd07b9f85ae23b48f8f
Jonathan de Flaugergues <jonathan.deflaugergues@abbeal.com>
2019-10-04 11:28:18 +0200

    sanitize tests

--- test.js
+++ test.js
@@ -8,7 +8,6 @@ test('responds to requests', (t) => {
     request('http://127.0.0.1:5000', (error, response, body) => {
       child.kill();
       t.false(error);
-      t.true(error);
       t.equal(response.statusCode, 200);
       t.notEqual(body.indexOf('Hello'), -1);
     });
