3da4c65960fa3e382117f27040476ab1858807ee
Jonathan de Flaugergues <jonathan.deflaugergues@abbeal.com>
2019-10-04 11:14:03 +0200

    update app to display failed test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello my third world\n');
+  res.end('Hello world on test failed\n');
 });
 
 server.listen(PORT, () => {
