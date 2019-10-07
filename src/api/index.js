6a3a7fb46be9081c74f524f5019d3f5fd07053d7
Jonathan de Flaugergues <jonathan.deflaugergues@abbeal.com>
2019-10-07 15:47:35 +0200

    fix bug

--- src/api/index.js
+++ src/api/index.js
@@ -2,43 +2,49 @@
 
 const Router = require('koa-router');
 const config = require('config');
+const mongoose = require('mongoose');
 
 const { User } = require('../models');
 
 const router = new Router();
 
-router.get('/ping', (ctx) => {
-  ctx.body = 'hello heroku OK';
-});
-
 router
+  .get('/ping', ping)
   .get('/users', getUsers)
+  .get('/users/create', createUser)
   .post('/users', createUser);
 
-async function getUsers(ctx) {
-  // const users = await User.find();
-  //
-  // ctx.body = users;
-  // ctx.status = 200;
+async function ping(ctx) {
+  ctx.body = 'hello heroku OK';
+}
 
-  ctx.body = config.database.url + ' \n' + process.env;
+async function getUsers(ctx) {
+  const users = await User.find();
+
+  ctx.body = users;
   ctx.status = 200;
 }
 
 async function createUser(ctx) {
   const user = {
-    firstName: 'John',
-    lastName: 'Doe',
+    firstName: ctx.query.firstName || 'John',
+    lastName: ctx.query.lastName || 'Doe',
     createdDate: new Date(),
   };
 
   User.init();
+
   await User.create(user)
     .then((createdUser) => {
-      ctx.body = createdUser;
+      console.log('ok created');
+      const jsonUser = createdUser.toObject();
+
+      ctx.body = jsonUser;
       ctx.status = 201;
     })
     .catch((err) => {
+      console.log('ok error')
+
       ctx.assert(
         err.code !== 11000,
         409,
@@ -46,6 +52,7 @@ async function createUser(ctx) {
       );
       ctx.assert(!err, 400, err, { code: err.name });
     });
+  ctx.status = 300;
 }
 
 module.exports = router;
